5e6a32b16d50959c1ae27bf4053102748429eeba
Full Name <email@adre.ss>
2017-08-16 15:43:22 +0200

    create project

--- SänkaSkepp/Program.cs
+++ SänkaSkepp/Program.cs
@@ -10,6 +10,66 @@ namespace SänkaSkepp
     {
         static void Main(string[] args)
         {
+            startGame();
+            PlayGame();
+            EndGame();
+
+
+
+
+
+        }
+
+        private static void EndGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PlayGame()
+        {
+            throw new NotImplementedException();
+            PrintField();
+            DropBomb();
+
+        }
+
+        private static void DropBomb()
+        {
+            throw new NotImplementedException();
+            CheckHit();
+            CheckAllHit();
+        }
+
+        private static void CheckAllHit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CheckHit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintField()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void startGame()
+        {
+            throw new NotImplementedException();
+            AskPlayerName();
+            PlaceShips();
+        }
+
+        private static void PlaceShips()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AskPlayerName()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ SänkaSkepp/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SänkaSkepp
+{
+    class Square
+    {
+        public bool isHit;
+        public bool isShip;
+        public string coords;
+
+    }
+}
