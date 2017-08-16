f0757e433e9b78c5dbe0755bd31459627d72cc91
akgjohansson <johansson.mcquack@gmail.com>
2017-08-16 15:57:21 +0200

    Added class GetInputFromUser

---
+++ SänkaSkepp/GetInputFromUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SänkaSkepp
+{
+    class GetInputFromUser
+    {
+        public int GetInt(string question)
+        {
+
+
+        }
+
+        public string GetString(string question)
+        {
+
+        }
+
+        public char GetChar(string question)
+        {
+
+        }
+    }
+}
