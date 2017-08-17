0b7b9bb40a9c61b5e08b308480245e53dc3268fe
akgjohansson <johansson.mcquack@gmail.com>
2017-08-17 08:07:01 +0200

    Finished method WriteGrid

--- SänkaSkepp/Grid.cs
+++ SänkaSkepp/Grid.cs
@@ -13,6 +13,7 @@ namespace SänkaSkepp
 
         private static void SetGrid()
         {
+            List<string> coords = WriteGrid();
             throw new NotImplementedException();
         }
 
@@ -21,7 +22,23 @@ namespace SänkaSkepp
             throw new NotImplementedException();
         }
 
-
+        private static List<string> WriteGrid() //returns coordinates of new grid
+        {
+            //Starting with rectangular grid
+            int rows = 4;
+            int columns = 4;
+            char letter = 'A';
+            List<string> coords = new List<string>();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    coords.Add($"{letter}{j}");
+                }
+                letter++;
+            }
+            return coords;
+        }
 
 
     }
