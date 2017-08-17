78d985fcca662871f2105247608e9e935757e500
akgjohansson <johansson.mcquack@gmail.com>
2017-08-17 15:52:39 +0200

    Lade till klassen Players

---
+++ SänkaSkepp/Players.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SänkaSkepp
+{
+    class Players
+    {
+        public Player player1 = new Player();
+        public Player player2 = new Player();
+    }
+}
